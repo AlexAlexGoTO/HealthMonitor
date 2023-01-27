ce4b1a73c197ac6a51221fde611369b23b20330c
Alex <alexkvitchastiy@gmail.com>
2023-01-27 02:24:46 +0200

    add types to table

--- MedixineMonitorClient/medixine-monitor-client/src/components/observations/Observation.jsx
+++ MedixineMonitorClient/medixine-monitor-client/src/components/observations/Observation.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 
 const Observation = ({observation}) => {
+    const types = {
+        "0": "Weight",
+        "1": "BloodPressure",
+        "2": "Pulse",
+        "3": "Steps",
+        "4": "BloodOxygenSaturation",
+    }
+
     return (
         <React.Fragment>
             <td>{observation.patientId}</td>
             <td>{observation.name}</td>
-            <td>{observation.type}</td>
+            <td>{types[observation.type]}</td>
             <td>{observation.value}</td>
             <td>{observation.description}</td>
         </React.Fragment>
